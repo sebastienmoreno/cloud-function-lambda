2f79e864e05045a27dc63f4bee58d7d3d007c724
Sébastien Moreno <sebastien.moreno@isocel.info>
2017-11-03 14:02:21 +0100

    projet Spring Function simple domain avec City et Food séparation fonction

---
+++ local-test.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+curl -H "Content-Type:application/json" localhost:8080/favoriteFunction -d '{"name":"Bordeaux"}'
---
+++ src/main/java/functions/Food.java
@@ -0,0 +1,18 @@
+package functions;
+
+public class Food {
+
+	private String name;
+
+	public Food(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(final String name) {
+		this.name = name;
+	}
+}
